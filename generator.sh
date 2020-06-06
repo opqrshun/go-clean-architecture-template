547426aff16ccfed6ff6dacbca48ac410c09a249
ttaki <opqrshun@gmail.com>
2020-06-07 00:24:53 +0900

    fix-shell

--- generator.sh
+++ generator.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR=sample
+SHELL_DIR=`dirname $0`
 # cp -r clean-architecture $PROJECT_DIR
 entity=$1
 Entity=${entity^}
+echo $Entity
 mkdir $PROJECT_DIR 
-rsync -av ./ $PROJECT_DIR/ # --exclude "entity*"
+rsync -av $SHELL_DIR/ $PROJECT_DIR/ # --exclude "entity*"
 
-sed -i "s/entity/${entity}/g" $PROJECT_DIR
-sed -i "s/Entity/${Entity}/g" $PROJECT_DIR
+sed -i "s/entity/${entity}/g" $(fd -e go --full-path $PROJECT_DIR ) 
+sed -i "s/Entity/${Entity}/g" $(fd -e go --full-path $PROJECT_DIR ) 
 
 find $PROJECT_DIR -name '*entity*' | xargs -L 1 rename entity ${entity}
